c13c1341b2f324e4013a8b04249875b2f225ff35
mikk1onfire <61505173+mikk1onfire@users.noreply.github.com>
2022-05-27 19:10:57 +0300

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
     <div className="footer-container">
       <p>2022 Mikki Market <br /> Все права защищены</p>
       <p className="icons">
-        <a href="https://instagram.com/mikki_nicki" target="_blank">
+        <a href="https://instagram.com/mikki_nicki">
           <AiFillInstagram/>
         </a>
-        <a href="https://twitter.com/mikki1596" target="_blank">
+        <a href="https://twitter.com/mikki1596">
           <AiOutlineTwitter />
         </a>
       </p>
